32a26019c31a95f464f586574c23dca23e37b38b
Ricardo de Vries <78354174+ricardodevries@users.noreply.github.com>
2026-03-17 23:58:43 +0100

    fix: Switch from CDN to local fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
@@ -26,4 +26,66 @@ export default defineConfig({
   adapter: node({
     mode: "standalone",
   }),
+  fonts: [
+    {
+      name: "IBM Plex Sans",
+      cssVariable: "--font-ibm-plex-sans",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: [
+              "./src/assets/fonts/IBMPlexSans-Regular.woff2",
+              "./src/assets/fonts/IBMPlexSans-Regular.ttf",
+            ],
+            weight: "400",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/IBMPlexSans-Italic.woff2"],
+            weight: "400",
+            style: "italic",
+          },
+          {
+            src: ["./src/assets/fonts/IBMPlexSans-Medium.woff2"],
+            weight: "500",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/IBMPlexSans-MediumItalic.woff2"],
+            weight: "500",
+            style: "italic",
+          },
+          {
+            src: [
+              "./src/assets/fonts/IBMPlexSans-SemiBold.woff2",
+              "./src/assets/fonts/IBMPlexSans-SemiBold.ttf",
+            ],
+            weight: "600",
+            style: "normal",
+          },
+          {
+            src: ["./src/assets/fonts/IBMPlexSans-SemiBoldItalic.woff2"],
+            weight: "600",
+            style: "italic",
+          },
+        ],
+      },
+    },
+    {
+      name: "Mono Lisa",
+      cssVariable: "--font-mono-lisa",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/MonoLisaVariableNormal.woff2"],
+          },
+          {
+            src: ["./src/assets/fonts/MonoLisaVariableItalic.woff2"],
+          },
+        ],
+      },
+    },
+  ],
 });
