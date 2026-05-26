1d1e175bdf69f861272ecb5dfba359b64cbd503c
Ricardo de Vries <78354174+ricardodevries@users.noreply.github.com>
2026-05-26 20:23:16 +0200

    refactor: Convert global CSS to SCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import svelte from "@astrojs/svelte";
+import svelte, { vitePreprocess } from "@astrojs/svelte";
 import node from "@astrojs/node";
 import { rehypePlugins, remarkPlugins } from "./config/plugins";
 import db from "@astrojs/db";
@@ -16,7 +16,12 @@ export default defineConfig({
     rehypePlugins,
     remarkPlugins,
   },
-  integrations: [mdx(), svelte({ preprocess: [] }), sitemap(), db()],
+  integrations: [
+    mdx(),
+    svelte({ preprocess: vitePreprocess() }),
+    sitemap(),
+    db(),
+  ],
   vite: {
     optimizeDeps: {
       exclude: ["@resvg/resvg-js"],
